6834ecdfde2210915e9b48308e19edd5b6e3d64a
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-21 15:49:55 -0400

    Pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringRev = '';
+    for (let i = string.length-1; i >= 0; i--) {
+        stringRev += string.substr(i, 1);
+    }
+    return stringRev;
 };
 
 // Do not edit below this line
