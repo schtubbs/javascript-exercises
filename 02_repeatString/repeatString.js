1a9cc75e3d5bb1305b2506a77aa63abf1731fb32
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-21 15:35:07 -0400

    Complete javascript exercise 07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num == 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    } else {
+        let newString;
 
+        for (let i = 0; i < num; i++) {
+            if (i == 0) {
+                newString = string;
+            } else {
+                newString += string;
+            }
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
