5e1d1eaf8cc06747fb612139cba5fbe1bc2b037c
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-21 16:19:50 -0400

    Add ability to remove multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,19 @@
 const removeFromArray = function(array, strToRemove) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === strToRemove) {
-            array.splice(i,1);
+    // Need to check for more than one removal string
+    let numArgs = arguments.length;
+    if (numArgs > 2) {
+        for (let a = 1; a < numArgs; a++) {
+            for (let i = 0; i < array.length; i++) {
+                if (array[i] === arguments[a]) {
+                    array.splice(i, 1);
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === strToRemove) {
+                array.splice(i,1);
+            }
         }
     }
     return array;
