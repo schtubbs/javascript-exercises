ed8fb158bfb3b320707607a9eef482eac9a41ecb
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 09:01:36 -0400

    Add errors to test if inputs are numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
 const sumAll = function(int1, int2) {
-    if (int1 < 0 || int2 < 0) {return "ERROR"}
+    if (
+        (typeof int1 !== "number" || typeof int2 !== "number") ||
+        (int1 < 0 || int2 < 0)
+    ) {
+        return "ERROR"
+    }
 
     let totalSum = 0;
     for (let i = int1; i <= int2; i++) {
