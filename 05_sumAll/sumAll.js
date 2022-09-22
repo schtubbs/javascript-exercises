bc6b3005c41047dde4f5cf303afe35f4d0d3488f
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-22 08:52:29 -0400

    Wrote initial function and pass first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let totalSum = 0;
+    for (let i = int1; i <= int2; i++) {
+        totalSum += i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
