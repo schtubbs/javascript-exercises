1af58af20eac60ad13e15123883c43c9b8e8a9bf
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-23 14:58:18 -0400

    Create palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strBackwards = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        strBackwards += str[i];
+        
+        console.log(strBackwards, str[i]);
+    }
+    return str === strBackwards;
 };
 
 // Do not edit below this line
