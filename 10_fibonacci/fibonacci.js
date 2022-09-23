5fa7cf08e6321ceb8772abae1c13017c9b54b990
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-23 15:34:42 -0400

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if (nth < 0 ) return "OOPS";
+    if (typeof nth === "string") nth = +nth;
+    
+    let fibArray = [0,1];
+    for (let i = 1; i < nth; i++) {
+        fibArray[i+1] = fibArray[i-1] + fibArray[i]
+    }
+    return fibArray[fibArray.length-1];
 };
 
 // Do not edit below this line
