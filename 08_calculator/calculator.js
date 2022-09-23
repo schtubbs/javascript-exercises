f4f00857ae9520f93faa61f290404c16998b7970
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-23 14:35:55 -0400

    Create all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let num in array) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  return num1 * num2;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = n;
+  for (let i = 1; i < n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
