0b755a3fc359bce3cf26a9ebcaf8d7a1015cd700
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-23 16:03:19 -0400

    Pass tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((prevPerson, nextPerson) => {
+        let prevAge;
+        let nextAge;
 
+        // Check if prev person has died then calculate age
+        if (prevPerson.yearOfDeath === undefined || prevPerson.yearOfDeath === null) {
+            prevAge = (new Date().getFullYear()) -  prevPerson.yearOfBirth;
+        }  else {
+            prevAge = prevPerson.yearOfDeath - prevPerson.yearOfBirth;
+        }
+
+        // Check if next person has died then calculate age
+        if (nextPerson.yearOfDeath === undefined || nextPerson.yearOfDeath === null) {
+            nextAge = (new Date().getFullYear()) - nextPerson.yearOfBirth;
+        } else {
+            nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        }
+        
+        return (prevAge > nextAge) ? prevPerson : nextPerson;
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
