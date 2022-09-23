5e3424e0ea5516aa7f76d6d1dc31b30d4f36182e
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-23 15:03:39 -0400

    Expand beyond single words without punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,11 @@
 const palindromes = function (str) {
+    str = str
+            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+            .replace(/\s/g,"")
+            .toLowerCase();
     let strBackwards = '';
     for (let i = str.length-1; i >= 0; i--) {
         strBackwards += str[i];
-        
-        console.log(strBackwards, str[i]);
     }
     return str === strBackwards;
 };
